3c4bf233e03bcbade6f8cf5ca2227df4b25557f2
Andrii Mishtal <mishtal.andriy@gmail.com>
2020-11-17 15:40:20 +0200

    Start project

---
+++ script.js
@@ -0,0 +1,7 @@
+
+
+
+// Format number as money - https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
+function formatMoney(number) {
+  return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
